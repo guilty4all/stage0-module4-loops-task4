69a90e890d20c7c9d234c7bcb945cd1feea2e122
guilt <gulilty4all@gmail.com>
2022-09-14 18:50:14 +0600

    some fixes4

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        for(int i = 0;i<=numberToGoUntil;i++){
+        for(int i = 0; i < numberToGoUntil; i++){
 
             if (i > toBreakWith){
                 break;
